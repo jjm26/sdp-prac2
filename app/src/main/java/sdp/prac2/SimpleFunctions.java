f976e2687d9e73b592c831d34d959cecf9025daa
tshegofatso01 <G17E4088@ict.ru.ac.za>
2022-07-21 16:13:56 +0200

    task 6 added

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -3,5 +3,28 @@ package sdp.prac2;
 import java.util.*;
 
 public class SimpleFunctions {
-    public SimpleFunctions() {}
+    public SimpleFunctions() {
+       //code goes here
+       
+        List<Integer> number = new ArrayList<Integer>();
+        List<Integer> results = new ArrayList<Integer>();
+    
+       
+       for (int i = 0; i < number.size(); i++) 
+       {
+           if(number.get(i) % 100 != 0)
+           {
+               int devide = (number.get(i) / 100 ) + 1;
+               results.add(devide * 100);
+               
+           }
+           else if(number.get(i) % 100 == 0) {
+               results.add(number.get(i));
+           }
+           
+       } 
+       
+       System.out.println(results);
+            
+     }
 }
