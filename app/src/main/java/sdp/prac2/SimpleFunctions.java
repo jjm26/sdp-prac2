97ff9d6287517e853ed8fb0eee623ed0ce1b249c
Kisaki77 <nobuhlemlahleki@gmail.com>
2022-07-21 15:23:08 +0200

    Task 5 by Kisaki

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -3,5 +3,14 @@ package sdp.prac2;
 import java.util.*;
 
 public class SimpleFunctions {
-    public SimpleFunctions() {}
+
+    public static boolean mySort(List<Integer> ListOfIntegers){
+        for (int i = 0; i < ListOfIntegers.size() -1; i ++){
+            if (ListOfIntegers.indexOf(i) > ListOfIntegers.indexOf(i +1)){
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
