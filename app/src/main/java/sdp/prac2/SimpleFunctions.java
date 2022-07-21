4d92c9bd5b6c1b78b67491dea445a35e2638710e
masterdoodles <masterdoodlestyler@gmail.com>
2022-07-21 14:28:10 +0200

    Task 2

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,16 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+    public List<String> tylerfunction(List<String> arrayList){
+        List<String> finalList = new ArrayList<String>();
+        for(int i =0; i<arrayList.size();i++){
+            
+           String _currentString = arrayList.get(i);
+           if(!_currentString.isEmpty()){
+                finalList.add(_currentString.substring(1));
+           }
+
+        }
+        return finalList;
+    }
 }
