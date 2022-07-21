fbf81c69e1592fbfdfee97b46ecd96e4e821f221
JJ <g20m7040@ru.ac.za>
2022-07-21 14:47:26 +0200

    Completed Task1

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -3,5 +3,18 @@ package sdp.prac2;
 import java.util.*;
 
 public class SimpleFunctions {
-    public SimpleFunctions() {}
+    public SimpleFunctions() {
+    }
+
+    public int Task1(List<Integer> a, List<Integer> b) {
+        int index = 0;
+        int sum = 0;
+        for (int i = 0; i < a.size(); i++) {
+            if (b.get(index) < a.size() - 1 && b.get(index)>-1) {
+                sum += a.get(b.get(index));
+            }
+            index++;
+        }
+        return sum;
+    }
 }
